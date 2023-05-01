f8fadce160a1802bcac1f84831cc0ac2d6407aaf
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-05-01 22:51:46 +0300

    feat: `task3` solution

--- test3/task3.js
+++ test3/task3.js
@@ -5,15 +5,27 @@ var rl = readline.createInterface({
 });
 let total = 0;
 process.stdin.on('end', () => { console.log(total); process.exit(0); });
-rl.on('line', function (data) {
-        data = Number(data);
-        let cut = 1;
-        let move = 0;
-        while(cut < data){
-          move++;
-          cut*=2;
-        }
-        total = move
-        console.log(total)
-        rl.close();
-});
+rl.on('line', (line1) => {
+  rl.on('line', (line2) => {
+    rl.on('line', (line3) => {
+      employers = line1.split(' ').filter(i => i!=='').map(i=>Number(i))
+      t = employers[1]
+      employers = employers[0]-1
+      levels = line2.split(' ').filter(i => i!=='').map(i=>Number(i))
+      faggot = (+line3)-1
+
+      if(levels[faggot] - levels[0] <= t || levels[levels.length-1] - levels[faggot] <= t) {
+        total = levels[employers] - levels[0]
+      }
+      else if (levels[employers] - levels[faggot] > levels[faggot] - levels[0]) {
+        total = levels[faggot] + levels[employers] - levels[0] * 2
+      }
+      else {
+        total = levels[employers] * 2 - levels[faggot] - levels[0]
+      }
+
+      console.log(total)
+      rl.close()
+    })
+  })
+})
